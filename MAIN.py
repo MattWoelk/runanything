ca752b1401651cd20b9d7e3e8991ef6d20eb66ef
Matthew Woelk <matt.woelk@gmail.com>
2012-04-12 23:53:26 -0500

    Simplifications and TODO list for project outline started.

--- MAIN.py
+++ MAIN.py
@@ -1,16 +1,23 @@
 import os
 import subprocess
 
-path = os.environ['PATH']
-paths = path.split(':')
+paths = os.environ['PATH'].split(':')
 programs = [] # List of all programs which could be auto-completed
-              # TODO: make it a dict with the corresponding path or something
 
 for i in paths:
   programs += os.listdir(i)
 
-#print(programs)
+# change the for loop to do an insertion sort (with a placeholder; super efficient) so the list is in order.
+# maybe to start just use the "sort" function
+programs = sorted(programs, key=str.lower) #success!
+print(programs)
 
+
+# TODO: make it a dictionary (tuples?) with the corresponding path or something
+# TODO: Interface with qt
+# TODO: - get input from the keyboard every time a key is hit
+# TODO: - check to see if there are any matches
+# TODO: - if so, show them in a list which key-bindings will highlight
 
 
 
@@ -20,6 +27,7 @@ for i in paths:
 ## Useful tools which may be useful to me later on in development:
 
 # This is used to launch a program in a new terminal which runs independently from this python program
+#term = os.environ['TERM'] # this is actually useless, (we can't run it from this information) so we'll have to set it in a config file :P
 #subprocess.Popen (['urxvt', '-e', 'sleep', '1']) 
 
 # This is used to launch a program which this python program waits for
